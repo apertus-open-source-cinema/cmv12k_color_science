e1382f6a11e625d655af6ac49b9a876d2861f1e9
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2022-02-13 18:44:56 +0100

    stop/start hdmi stream before capturing snaps

--- capture/capture_calibration_frame.sh
+++ capture/capture_calibration_frame.sh
@@ -12,7 +12,10 @@ ssh root@$1 "axiom_snap -2 -b -r -e $2 -z"
 
 # capture one raw snap before the averaging sequence to log the image sensor temperature before 
 echo "capturing 1 raw snap"
+ssh root@$1 "axiom_sequencer_stop.sh"
 ssh root@$1 "axiom_snap -2 -b -r -e $2" > snap01_$2.raw12
+ssh root@$1 "axiom_sequencer_start.sh"
+
 
 
 #capture sequence
@@ -22,9 +25,11 @@ for time in 1; do cargo run --release -- WebcamInput --device 0 ! DualFrameRawDe
 
 #capture snaps
 echo "capturing 3 more raw snaps"
+ssh root@$1 "axiom_sequencer_stop.sh"
 ssh root@$1 "axiom_snap -2 -b -r -e $2" > snap02_$2.raw12
 ssh root@$1 "axiom_snap -2 -b -r -e $2" > snap03_$2.raw12
 ssh root@$1 "axiom_snap -2 -b -r -e $2" > snap04_$2.raw12
+ssh root@$1 "axiom_sequencer_start.sh"
 
 cd ..
 
